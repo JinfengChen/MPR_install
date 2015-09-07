23e874896d462687c840da7bb6310e2e46b100fb
JinfengChen <jinfeng7chen@gmail.com>
2015-09-07 16:42:38 -0700

    install on pigeon

--- MPR_genotyping/MPR.sh
+++ MPR_genotyping/MPR.sh
@@ -2,10 +2,11 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=10gb
 #PBS -l walltime=100:00:00
+#PBS -d ./
 
-export R_LIBS="/rhome/cjinfeng/software/tools/R_package_MPR/"
-cd $PBS_O_WORKDIR
+export R_LIBS="/bigdata/stajichlab/cjinfeng/software/R_package_MPR/"
+#cd $PBS_O_WORKDIR
 
-cat MPR_run.R | /rhome/cjinfeng/software/tools/R-2.15.3/bin/R --slave 
+cat MPR_run.R | R --slave 
 
 echo "Done"
