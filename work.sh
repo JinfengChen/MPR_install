e8f8fc55bb0b0e25c6981ec5a35003a3c1bf1bb4
JinfengChen <jinfeng7chen@gmail.com>
2015-09-07 16:35:59 -0700

    install on pigeon

--- work.sh
+++ work.sh
@@ -1,23 +1,20 @@
 echo "use a new dir for all MPR-related package"
-export R_LIBS=$R_LIBS:"/rhome/cjinfeng/software/tools/R_package_MPR/"
+export R_LIBS=$R_LIBS:"/bigdata/stajichlab/cjinfeng/software/R_package_MPR/"
 
 echo "install Biocgeneric"
-/rhome/cjinfeng/software/tools/R-2.15.3/bin/R CMD INSTALL BiocGenerics_0.10.0.tar.gz
+#R.3.2.0
+R CMD INSTALL BiocGenerics_0.10.0.tar.gz
 
 echo "install Biobase"
-/rhome/cjinfeng/software/tools/R-2.15.3/bin/R
+#R.3.2.0, update all other package required, yes
+R
 source("http://bioconductor.org/biocLite.R")
 biocLite("Biobase")
 
 echo "install MPR"
-/rhome/cjinfeng/software/tools/R-2.15.3/bin/R
-source("http://bioconductor.org/biocLite.R")
-biocLite("genefilter")
-biocLite("annotate")
-biocLite("AnnotationDbi")
-
+#R.3.2.0
 cd MPR_genotyping
-/rhome/cjinfeng/software/tools/R-2.15.3/bin/R CMD INSTALL MPR_0.1.tar.gz
+R CMD INSTALL MPR_NAMESPACE_0.1.tar.gz
 qsub MPR.sh
 
 
